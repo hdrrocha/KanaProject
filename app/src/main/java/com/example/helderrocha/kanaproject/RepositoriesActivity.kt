d45ab59425fe6b6086685c9c716d31c01fac6fcf
Heder Rocha <hedie.strokes@gmail.com>
2018-08-20 20:24:58 -0300

    push initial

---
+++ app/src/main/java/com/example/helderrocha/kanaproject/RepositoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helderrocha.kanaproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RepositoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.repositories_activity)
+    }
+}
